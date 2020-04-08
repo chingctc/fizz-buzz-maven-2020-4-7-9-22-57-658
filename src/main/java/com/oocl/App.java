4e4c1365010938b44ecb6664b686bddbb3df8fd5
Mavis Chan <mavis.chan@oocl.com>
2020-04-08 16:02:15 +0800

    [Mavis] - Passed test case should_return_number_when_normal_number_is_inputted

--- src/main/java/com/oocl/App.java
+++ src/main/java/com/oocl/App.java
@@ -6,8 +6,13 @@ package com.oocl;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+
+    public String FizzBuzz(int sequenceNumber) {
+        return String.valueOf(1);
     }
 }
+
--- src/test/java/com/oocl/AppTest.java
+++ src/test/java/com/oocl/AppTest.java
@@ -1,11 +1,23 @@
 package com.oocl;
 
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
 
 public class AppTest {
     @Test
     public void helloWorld() {
 
+    }
+
+    @Test
+    public void should_return_number_when_normal_number_is_inputted() {
+        App app = new App();
+        String result = app.FizzBuzz(1);
+
+        Assert.assertEquals("1", result);
     }
 }
